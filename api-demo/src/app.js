d6d61a93265270b560da967fd892e1222c938d0a
brahiangomezrivera238-art <brahiangomezrivera238@gmail.com>
2026-05-29 21:15:42 -0500

    Organizando la estructura en una carpeta api-demo

---
+++ api-demo/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path'); // Nueva línea: ayuda a manejar rutas de carpetas
+const rutasProductos = require('./routes/productos');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// NUEVO: Servir archivos estáticos desde la carpeta 'public'
+// Esto permite que el navegador encuentre tu index.html y tus estilos
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Rutas de la API
+app.use('/api/productos', rutasProductos);
+
+module.exports = app;
+
+// Cambios realizados por Samuel Molina - Actividad Clase Mayo
